c634936751753651d97a6c439017a091bf6922a6
k-siganski <61959576+k-siganski@users.noreply.github.com>
2020-04-09 16:19:55 -0600

    outline of functions for table access

--- myAddon/lua/autorun/server/server.lua
+++ myAddon/lua/autorun/server/server.lua
@@ -1,1 +1,8 @@
 --save in lua\autorun\server
+--include functions for creating and managing the table
+include(songTable.lua);
+creatTable();
+addSong();
+removeSong();
+getSong();
+--create hook to watch the chat with
---
+++ myAddon/lua/autorun/server/songTable.lua
@@ -0,0 +1,17 @@
+--creating the table
+function createTable()
+    print( "Creating the song table!" );
+end
+--adding a song onto the table
+function addSong()
+    print( "Adding a song to the table!" );
+end
+
+--remove song from end of table
+function removeSong()
+    print( "Removing a song from the table!" );
+end
+
+function getSong()
+    print( "Getting a song from the table!" );
+end
