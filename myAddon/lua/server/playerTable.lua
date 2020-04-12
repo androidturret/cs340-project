1823f3f2cba4e861b7912f40c0b4046bde7669d7
k-siganski <61959576+k-siganski@users.noreply.github.com>
2020-04-11 18:00:09 -0600

    adding playertable file, fixing songtable location

---
+++ myAddon/lua/server/playerTable.lua
@@ -0,0 +1,13 @@
+-- creating the table
+players={}
+
+--removePlayer and addPlayer are already gmod functions. want to avoid any possible overlap
+--adding a player to the table
+function includePlayer( p, message, teamChat )
+    print ("Player has been added!");
+end
+
+--removing a player from the table
+function excludePlayer( p, message, teamChat )
+    print("Player has be removed!");
+end
--- myAddon/lua/server/songTable.lua
+++ myAddon/lua/server/songTable.lua
@@ -1,17 +1,35 @@
 --creating the table
-function createTable()
-    print( "Creating the song table!" );
-end
+songs={}
+
 --adding a song onto the table
-function addSong()
-    print( "Adding a song to the table!" );
+function addSong( p, message, teamChat )
+
+    if string.match(message, "!addsong") then
+        print( "Adding a song to the table!" );
+        
+        --remove the comand so we're left with the url
+        url = string.Replace(message, "!addsong ", "")
+
+        --insert will always append to the end
+        table.insert( songs, url );
+    end
 end
 
 --remove song from end of table
-function removeSong()
-    print( "Removing a song from the table!" );
+function removeSong(p, message, teamChat)
+
+    if string.match(message, "!removesong") then
+        print( "Removing a song from the table!" );
+        -- will always remove last index of the table but if you give it the index of 1
+        -- it removes the first element instead which is technically the oldest since always adding to the end
+        --make sure table isn't empty before removing from it
+        if !table.IsEmpty(songs) then
+            print("entry removed!");
+            table.remove(songs, 1);
+        end
+    end
 end
 
-function getSong()
-    print( "Getting a song from the table!" );
+function getSong( index )
+    return songs[index];
 end
