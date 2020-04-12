d771b5d7537d77e4b8003a57fa19ab786ad530ae
k-siganski <61959576+k-siganski@users.noreply.github.com>
2020-04-11 19:45:40 -0600

    Creating option to opt in and out of radio
    Note: fix loop to find user in remove function

--- myAddon/lua/autorun/server/server.lua
+++ myAddon/lua/autorun/server/server.lua
@@ -1,11 +1,20 @@
 --save in lua\autorun\server
 --include functions for creating and managing the table
 include( "server/songTable.lua" );
+include ( "server/playerTable.lua" );
 
 --create hooks to watch the chat with
 --adding songs
-
 hook.Add("PlayerSay", "addsongtoqueue", addSong);
 
 --removing songs
 hook.Add("PlayerSay", "removesongfromqueue", removeSong);
+
+--including players
+hook.Add("PlayerSay", "playeroptin", includePlayer);
+
+--removing players
+hook.Add("PlayerSay", "playeroptout", excludePlayer);
+
+-- for testing only. Remove when done
+hook.Add("PlayerSay", "playerlistcheck", printPlayer);
--- myAddon/lua/server/playerTable.lua
+++ myAddon/lua/server/playerTable.lua
@@ -4,10 +4,38 @@ players={}
 --removePlayer and addPlayer are already gmod functions. want to avoid any possible overlap
 --adding a player to the table
 function includePlayer( p, message, teamChat )
-    print ("Player has been added!");
+    if string.match(message, "!joinradio") then
+
+        --adding the player's userID to the table
+        table.insert( players, p:UserID());
+
+        print ("Player ", p, " has been added!");
+    end
 end
 
 --removing a player from the table
 function excludePlayer( p, message, teamChat )
-    print("Player has be removed!");
+    if string.match(message, "!leaveradio") then
+        -- need to loop through the table until we find the matching value
+        local thisPlayer = p:UserID();
+        local key = nil
+        --to fix. loop currently doesn't work
+        for k,v in players(thisPlayer) do
+            if v==thisPlayer then key = k end
+        end
+
+        if key == nil then 
+            print("player ", p:Nick(), "Has not opted in yet and cannot be removed");
+        else
+            table.remove(songs, key);
+            print("Player ", p.Nick(), " has be removed!");
+        end
+    end
+end
+
+
+function printPlayer ( p, message, teamchat ) 
+    print(table.ToString(players));
+    print("test", p:Nick(), "test2", p:UserID() );
+    print( Player(3) );
 end
